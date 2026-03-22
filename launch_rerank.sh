769c2d51b443fb0534526bec59f2e3d9e162b146
rfaulkner <rfaulk@cs.toronto.edu>
2026-03-22 16:24:18 -0400

    Add some options to rerank launch.

--- launch_rerank.sh
+++ launch_rerank.sh
@@ -19,8 +19,8 @@ module load python/3.11.5 cuda/12.2 gcc arrow/21.0.0
 cd $project_dir
 source .venv/bin/activate
 
-# gpt-4.1-2025-04-14 gpt-4o-2024-05-13
-python -m icl.run_reranking --dataset semeval_en --tokenizer-model bert-base-uncased --llm-model gpt4 --llm-checkpoint gpt-4o-2024-05-13  --n-icl-examples 16
-# python -m icl.run_reranking --dataset semeval_en --tokenizer-model bert-base-uncased --llm-model qwen --llm-checkpoint Qwen/Qwen2.5-72B-Instruct --n-icl-examples 8
+# gpt-4.1-2025-04-14 gpt-4o-2024-05-13   Qwen/Qwen3-4B-Thinking-2507  Qwen/Qwen2.5-72B-Instruct
+# python -m icl.run_reranking --dataset semeval_en --tokenizer-model bert-base-uncased --llm-model gpt4 --llm-checkpoint gpt-4o-2024-05-13  --n-icl-examples 16
+python -m icl.run_reranking --dataset semeval_en --tokenizer-model bert-base-uncased --llm-model qwen --llm-checkpoint Qwen/Qwen3-4B-Thinking-2507 --n-icl-examples 4
 # python -m icl.run_reranking --dataset semeval_en --tokenizer-model bert-base-uncased --llm-model gemma3 --llm-checkpoint google/gemma-3-4b-it --scaling-curve --bucket-sizes 0,1,2,4,8,16 --n-bucket-seeds 1
 
